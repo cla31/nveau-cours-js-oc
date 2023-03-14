c0a454faef9d4b030c484c49f55ec353fb2c5678
Claire_M <clairerodriguez79@yahoo.fr>
2023-03-14 16:51:49 +0100

    Using local storage: add API response, remove + Show reviews from local storage

--- pieces.js
+++ pieces.js
@@ -1,7 +1,41 @@
-import { ajoutListenersAvis, ajoutListenerEnvoyerAvis } from "./avis.js";
+import { ajoutListenersAvis, ajoutListenerEnvoyerAvis, afficherAvis } from "./avis.js";
 
+// Récupération des pièces éventuellement stockées dans le localStorage
+let pieces = window.localStorage.getItem("pieces");
+/* Code de récupération des pièces depuis l’API HTTP */
+// Récupération des pièces depuis l'API
 const reponse = await fetch('http://localhost:8081/pieces/');
-const pieces = await reponse.json();
+pieces = await reponse.json();
+// Transformation des pièces en JSON
+const valeurPieces = JSON.stringify(pieces);
+// Stockage des informations dans le localStorage
+window.localStorage.setItem("pieces", valeurPieces);
+//Foireux:
+// if (pieces === null) {
+//     /* Code de récupération des pièces depuis l’API HTTP */
+//     // Récupération des pièces depuis l'API
+//     const reponse = await fetch('http://localhost:8081/pieces/');
+//     pieces = await reponse.json();
+//     // Transformation des pièces en JSON
+//     const valeurPieces = JSON.stringify(pieces);
+//     // Stockage des informations dans le localStorage
+//     window.localStorage.setItem("pieces", valeurPieces);
+// } else {
+//     pieces = JSON.parse(pieces);
+// }
+
+// const reponse = await fetch('http://localhost:8081/pieces/');
+// const pieces = await reponse.json();
+//équivalent avec then
+// const pieces = await fetch("http://localhost:8081/pieces").then(pieces => pieces.jon());
+
+// Transformation des pièces en JSON
+// const valeurPieces = JSON.stringify(pieces);
+
+// Stockage des informations dans le localStorage
+window.localStorage.setItem("pieces", valeurPieces);
+
+
 // on appelle la fonction pour ajouter le listener au formulaire
 ajoutListenerEnvoyerAvis()
 
@@ -52,6 +86,17 @@ function genererPieces(pieces) {
 
 }
 genererPieces(pieces);
+for (let i = 0; i < pieces.length; i++) {
+    const id = pieces[i].id;
+    const avisJSON = window.localStorage.getItem(`avis-piece-${id}`);
+    const avis = JSON.parse(avisJSON);
+
+    if (avis !== null) {
+        const pieceElement = document.querySelector(`article[data-id="${id}"]`);
+        afficherAvis(pieceElement, avis)
+    }
+}
+
 //gestion des bouttons 
 const boutonTrier = document.querySelector(".btn-trier");
 
@@ -156,3 +201,10 @@ inputPrixMax.addEventListener('input', function() {
     document.querySelector(".fiches").innerHTML = "";
     genererPieces(piecesFiltrees);
 })
+
+
+// Ajout du listener pour mettre à jour des données du localStorage
+const boutonMettreAJour = document.querySelector(".btn-maj");
+boutonMettreAJour.addEventListener("click", function() {
+    window.localStorage.removeItem("pieces");
+});
